9cbb71d7917fd217f38ac18cd44f96c2bdaf8b87
Jane-Code-cloud <1215174693@qq.com>
2026-08-04 23:35:06 +0800

    【微服务练习】seata功能,存在一点依赖问题

---
+++ SpringCloud/cloud-demo/services/seata-order/src/main/java/com/cyy/order/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.cyy.order.service;
+
+import com.cyy.order.bean.OrderTbl;
+
+public interface OrderService {
+    /**
+     * 创建订单
+     * @param userId    用户id
+     * @param commodityCode  商品编码
+     * @param orderCount  商品数量
+     */
+    OrderTbl create(String userId, String commodityCode, int orderCount);
+}
